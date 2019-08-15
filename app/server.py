6ce7f4d1e089b06611de1b94c3e44f45ddb120b0
Paul Bradshaw <paulonhismobile@gmail.com>
2019-08-15 11:26:22 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,16 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+#This is the URL for the original bears app
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#We redefine it for the new model created
+export_file_url = 'https://www.dropbox.com/s/fgmdumra1c1hodr/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
+#This is the list of classes for the original bears app
 classes = ['black', 'grizzly', 'teddys']
+#We redefine it for the new model
+classes = ['ironman', 'cyborg', 'terminator', 'robocop']
 path = Path(__file__).parent
 
 app = Starlette()
